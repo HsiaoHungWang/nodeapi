24477b4deee62aa45672336d3f90193e34e959ce
HsiaoHungWang <wang@ispan.com.tw>
2024-11-21 09:54:33 +0800

    第一次 commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# README
+## 建立資料庫環境
+* 新增資料庫：mydb 
+* 下載 mysql_mydb.sql
+ https://ispancomtw-my.sharepoint.com/personal/wang_ispan_com_tw/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fwang%5Fispan%5Fcom%5Ftw%2FDocuments%2F%E4%B8%8A%E8%AA%B2%E5%88%86%E4%BA%AB%2Fmysql%5Fmydb%2Esql&parent=%2Fpersonal%2Fwang%5Fispan%5Fcom%5Ftw%2FDocuments%2F%E4%B8%8A%E8%AA%B2%E5%88%86%E4%BA%AB&ga=1
+
+* 執行下載的 mysql_mydb.sql
+
+## 安裝 API
+* 複製專案 git clone 
+* 進入專案資料夾 cd nodeapi
+* 安裝套件 npm install
---
+++ routes/api.js
@@ -0,0 +1,159 @@
+var express = require('express');
+var router = express.Router();
+const mysql = require('mysql2/promise');
+
+
+//MySQL 連線設定
+const connectionString = {
+    host:'localhost',
+    user:'root',
+    password:'P@ssw0rd',
+    database: 'mydb'
+}
+
+
+router.get('/', function(req, res, next) {
+  res.json({ title: 'RESTful API' });
+});
+
+//http://localhost:3000/api/categories
+router.get('/categories',async(req, res)=>{
+    let connection;
+    try{
+        connection = await mysql.createConnection(connectionString);
+        const[rows] = await connection.execute('SELECT * FROM categories');
+        res.json(rows)
+    }catch(error){
+        console.log('資料庫連線失敗：', error);
+        res.status(500).send('資料庫錯誤...')
+    }finally{
+        if(connection){
+            await connection.end();
+        }
+    }
+})
+
+router.get('/categories/:id', async (req, res) => {
+    const { id } = req.params;
+    let connection;
+    try {
+      connection = await mysql.createConnection(connectionString);
+      const [rows] = await connection.execute('SELECT * FROM categories WHERE CategoryId = ?', [id]);
+      if (rows.length > 0) {
+        res.json(rows[0]);
+      } else {
+        res.status(404).send('查無此分類資料');
+      }
+    } catch (error) {
+      console.error('資料庫連線失敗:', error);
+      res.status(500).send('資料庫錯誤...');
+    } finally {
+      if (connection) {
+        await connection.end();
+      }
+    }
+  });
+
+
+router.post('/categories', async (req, res) => {
+    const { CategoryName } = req.body;
+    let connection;
+    try {
+      connection = await mysql.createConnection(connectionString);
+      const [result] = await connection.execute('INSERT INTO categories (CategoryName) VALUES (?)', [CategoryName]);
+      res.status(201).json({ id: result.insertId, CategoryName});
+    } catch (error) {
+      console.error('資料庫連線失敗:', error);
+      res.status(500).send('資料庫錯誤...');
+    } finally {
+      if (connection) {
+        await connection.end();
+      }
+    }
+  });
+
+router.delete('/categories/:id', async (req, res) => {
+    const { id } = req.params;
+    let connection;
+    try {
+      connection = await mysql.createConnection(connectionString);
+      const [result] = await connection.execute('DELETE FROM categories WHERE CategoryId = ?', [id]);
+      if(result.affectedRows > 0){
+        res.status(204).send()
+      }else{
+        res.status(404).send('找不到要刪除的分類資料');
+      }
+    } catch (error) {
+      console.error('資料庫連線失敗:', error);
+      res.status(500).send('資料庫錯誤...');
+    } finally {
+      if (connection) {
+        await connection.end();
+      }
+    }
+  });
+
+  router.put('/categories/:id', async (req, res) => {
+    const { id } = req.params;
+    const { CategoryName } = req.body;
+    let connection;
+    try {
+      connection = await mysql.createConnection(connectionString);
+      const [result] = await connection.execute('UPDATE categories SET CategoryName = ? WHERE CategoryId = ?', [CategoryName, id]);
+      if (result.affectedRows > 0) {
+        res.json({ id, CategoryName });
+      } else {
+        res.status(404).send('找不到要修改的分類資料');
+      }
+    } catch (error) {
+      console.error('資料庫連線失敗:', error);
+      res.status(500).send('資料庫錯誤...');
+    } finally {
+      if (connection) {
+        await connection.end();
+      }
+    }
+  });
+
+
+  router.get('/spots', async (req, res) => {
+    const { keyword, categoryId, sortBy, sortType, page, pageSize } = req.query;
+  
+     // 建立查詢語句
+  let query = 'SELECT * FROM spotimagesspot WHERE 1=1';
+  const params = [];
+  if (keyword) {
+    query += ' AND (SpotTitle LIKE ? OR SpotDescription LIKE ?)';
+    params.push(`%${keyword}%`,`%${keyword}%`);
+
+  }
+  
+  if (categoryId) {
+    query += ' AND CategoryId = ?';
+    params.push(categoryId);
+  }
+
+  // 排序
+  if (sortBy) {
+    query += ` ORDER BY ${sortBy} ${sortType === 'asc' ? 'ASC' : 'DESC'}`;
+  }
+
+//   分頁
+  const limit = (parseInt(pageSize, 10) || 10).toString();
+  const offset = ((parseInt(page, 10) - 1) * limit).toString(); 
+
+  query += ' LIMIT ? OFFSET ?';
+
+
+
+let connection;
+  try {
+    connection = await mysql.createConnection(connectionString);
+    const [results] = await connection.execute(query, params);
+    res.json(results);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+  });
+
+module.exports = router;
